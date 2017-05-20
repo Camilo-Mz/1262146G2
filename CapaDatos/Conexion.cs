3483e5af12e8fb7b9011929fb10b1b1fe83ac422
sluguer@gmail.com <sluguer@gmail.com>
2017-05-19 20:59:13 -0500

    FUNCIONANDO CONEXION CON LA BD EN EL FORMULARIO LOGIN

--- CapaDatos/Conexion.cs
+++ CapaDatos/Conexion.cs
@@ -9,8 +9,38 @@ using System.Windows.Forms;
 
 namespace CapaDatos
 {
-    class Conexion
+    public class Conexion
     {
+        private string con = @"Data Source=ING-SLUGUER;Initial Catalog=Ejercicio1;Integrated Security=True";
+        //Data Source = ING - SLUGUER; Initial Catalog = PruebaLoginRol; Integrated Security = True
+        //encapsular la variable privada (con)// 
+        public string Cnn
+        {
+            get { return con; }
+            set { value = con; }
+        }
+
+        //crear la variable para la conexion con sql server 
+        public SqlConnection conec;
+
+        //crear el metodo para la conexion 
+        public SqlConnection conecte()
+        {
+            try
+            {
+                //instancia de la clase sqlconnection 
+                conec = new SqlConnection(Cnn);
+                conec.Open();
+                return conec;
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error al conectar " + ex);
+                conec.Close();
+                return conec;
+            }
+        }
 
 
     }
--- CapaLogica/Login.cs
+++ CapaLogica/Login.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaDatos;
 
- namespace CapaLogica
+namespace CapaLogica
 {
     public class Login
     {
-        public void ValidarUsuario(string usuario, string clave)
+
+        public SqlDataReader lector;
+        public SqlCommand comando;
+        public String ValidarUsuario(string usuario, string contraseña)
         {
-          
+            Conexion oConexion = new Conexion();
+            try
+            {
+                comando = new SqlCommand("select idRol from Usuario where nickname = '" + usuario + "' and clave = '" + contraseña + "'", oConexion.conecte());
+                lector = comando.ExecuteReader();
+                if (lector.Read())
+                {
+                    MessageBox.Show("usuario y contraseña validos");
+
+                    return lector[0].ToString();
+                    
+
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña Incorrectos");
+                    return "";
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error al conectar " + ex);
+                return "";
+            }
         }
     }
 }
